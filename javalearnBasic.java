2ac2c2d9ad51613ffd9d70272a33e31648ea9469
Your GitHub Username <cliftondsouza2006@gmail.com>
2025-11-06 23:04:09 +0530

    java

--- javalearnBasic.java
+++ javalearnBasic.java
@@ -1,5 +1,28 @@
+class calculator{
+     
+    public void pen(){
+        System.out.println("Pen is moving");
+    }
+
+    public String cost(int cost){
+        if(cost >= 15){
+            return "pen";
+        }else{
+            return "Nothing";
+        }
+        
+    }
+}
+
+
+
+
+
 public class javalearnBasic{
     public static void main(String[] args){
-        System.out.println("hi");
+        calculator calc = new calculator();
+        String s = calc.cost(5);
+        calc.pen();
+        System.out.println(s);
     }
 }
